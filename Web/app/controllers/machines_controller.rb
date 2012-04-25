ea3ee9f37cb70bfab72d59b8484e60a6db6c0130
Ross Frazier <rossfrazier@gmail.com>
2012-04-24 21:30:50 -0400

    testing

--- Web/app/controllers/machines_controller.rb
+++ Web/app/controllers/machines_controller.rb
@@ -2,7 +2,12 @@ class MachinesController < ApplicationController
   def update
     machine = Machine.find(params[:id])
     machine.update_attributes(:active=>params[:machine][:active], :ip_address=>params[:machine][:ip_address])
+    machine.reset_chomper!
     flash[:info] = "Machine status updated."
     redirect_to :back
+  end
+
+  def create
+  	render :status => 200
   end
 end
