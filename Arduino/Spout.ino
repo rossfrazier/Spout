d92f0a83882747c0be7f3836d9de03aa5d7ec0ca
Ross Frazier <rossfrazier@gmail.com>
2012-06-03 16:56:31 -0400

    test functions to control pump and conveyer motors
    accessed at http://x.x.x.x/spout/test with a post request

--- Arduino/Spout.ino
+++ Arduino/Spout.ino
@@ -3,6 +3,7 @@
 #include "WebServer.h"
 #include "Pour.h"
 #include "Drink.h"
+#include "Test.h"
 
 // MAC ADDRESS
 static uint8_t mac[6] = { 0x90, 0xA2, 0xDA, 0x00, 0xC2, 0xB3 };
@@ -17,8 +18,11 @@ static uint8_t ip[4] = { 192, 168, 1, 10 };
 WebServer webserver(PREFIX, 80);  //second param is port value
 
 #define MAX_INGREDIENTS 8
+#define BUFFER_SIZE 4
+
+//default command: takes an array of drink ingredients
 void newDrinkCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) {
-    static const byte bufferSize = 4;
+    static const byte bufferSize = BUFFER_SIZE;
     if (type == WebServer::POST) {
         bool hasMoreParams;
         byte poursCount = 0;
@@ -67,8 +71,43 @@ void newDrinkCmd(WebServer &server, WebServer::ConnectionType type, char *, bool
     }
 }
 
+//testing methods: can be used to arbitrarily control air pumps, conveyers, etc.
+//reads POST params in the same way as in the default command.
+void testCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) {
+    static const byte bufferSize = BUFFER_SIZE;
+    if (type == WebServer::POST) {
+        bool hasMoreParams;
+        char name[bufferSize], value[bufferSize];
+
+        do {
+            hasMoreParams = server.readPOSTparam(name, bufferSize, value, bufferSize);
+            int secondsRunning = strtoul(value, NULL, 10);
+            //p prefix on post param name means the user wants to run the pump
+            if (strncmp(name, "p", 1) == 0) {
+                Test::runPumpForTime(secondsRunning);
+            }
+            //c prefix on post param name means the user wants to run the conveyer motor
+            else if (strncmp(name, "c", 1) == 0 {
+                Test::runConveyerForTime(secondsRunning);
+            }
+            else {
+                httpFail();
+                return;
+            }
+        } while (hasMoreParams);
+
+        server.httpSuccess();
+    }
+    else {
+        server.httpFail();
+    }
+
+    return;
+}
+
 void setup() {
     //setup pins as input/output; sizeOf works for counting elements because it's an array of bytes
+    pinMode(Test::pumpTransistor, OUTPUT);
     for (byte i = 0; i < sizeof(Drink::valveTransistorPins); i++) {
         pinMode(Drink::valveTransistorPins[i], OUTPUT);
     }
@@ -78,9 +117,12 @@ void setup() {
     Serial.begin(9600);
     Serial.println("program initialized");
     
+    //web routes
     /* register our webserver default command (activated with the request of
      * http://x.x.x.x/spout */
     webserver.setDefaultCommand(&newDrinkCmd);
+    //test commands use POST url of http://x.x.x.x/spout/test
+    webserver.addCommand("/test",&testCmd);
     
     // start the server to wait for connections
     webserver.begin();
---
+++ Arduino/Test.cpp
@@ -0,0 +1,38 @@
+//
+//  Test.cpp
+//  Spout
+//
+//  Implementation for the Test object (detailed description in the header file).
+
+#include "Test.h"
+
+void Test::controlPump(motorStatus_t pumpStatus) {
+	if (pumpStatus == ON) {
+		digitalWrite(Test::pumpTransistor, HIGH);
+	}
+	else if (pumpStatus == OFF) {
+		digitalWrite(Test::pumpTransitor, LOW);
+	}
+}
+
+void Test::runPumpForTime(int seconds) {
+	controlPump(ON);
+	delay(seconds*1000);
+	controlPump(OFF);
+}
+
+//still needs code to control the motor -- analogWrite, probably.
+void Test::controlConveyer(motorStatus_t motorStatus) {
+	if (motorStatus == ON) {
+
+	}
+	else if (motorStatus == OFF) {
+
+	}
+}
+
+void Test::runConveyerForTime(int seconds) {
+	controlConveyer(ON);
+	delay(seconds*1000);
+	controlConveyer(OFF);
+}
