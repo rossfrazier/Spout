9b21f56ba0549775bc3a034e7479076750c723dd
Ross Frazier <rossfrazier@gmail.com>
2012-06-30 13:40:56 -0400

    moving web interface to a different repo

--- Web/app/assets/javascripts/navbar.coffee
+++
@@ -1,4 +0,0 @@
-jQuery ->
-	$(document).ready ->
-		activeController = $('#main-navbar').data('active');
-		$('#main-navbar li#'+activeController).addClass('active');
--- Web/app/models/pour.rb
+++
@@ -1,9 +0,0 @@
-class Pour < ActiveRecord::Base
-  #a pour is an ingredient (which bottle?) and a volume (or pouring time) of that ingredient
-  belongs_to :drink, :dependent=>:destroy
-  belongs_to :ingredient
-
-  def bottle
-    self.ingredient.bottle
-  end
-end
