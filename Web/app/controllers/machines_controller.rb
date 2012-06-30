a3fbef969c3067b407a5cf1885fc76e2968e6fec
Ross Frazier <rossfrazier@gmail.com>
2012-06-30 13:38:53 -0400

    organizing

--- Web/app/controllers/machines_controller.rb
+++ Web/app/controllers/machines_controller.rb
@@ -1,24 +1,32 @@
 class MachinesController < ApplicationController
-  def index
+  before_filter :assign_machine
+
+  def assign_machine
     @machine = $machine
+  end
+  
+  def index
   end
 
   def create
-    $machine.start
+    @machine.start
     flash[:success] = "Drink order queue now active."
     redirect_to :back
   end
 
   def destroy
-    $machine.stop
+    @machine.stop
     flash[:error] = "Drink order queue now deactivated."
     redirect_to :back
   end
 
   def update
-    $machine.ip_address = params[:ip_address]
+    @machine.ip_address = params[:ip_address]
     flash[:success] = "Spout web connection address updated to "+$machine.uri.to_s
-    redirect_to :back
+    respond_to do |format|
+      format.html {redirect_to :back}
+      format.json {render :nothing=>true}
+    end
   end
 
 end
