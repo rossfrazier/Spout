59726d872dec3b6ed98288e43ec8c865d2f840e2
Ross Frazier <rossfrazier@gmail.com>
2012-12-19 22:28:01 -0500

    adding IR sensor logic
    the machine uses an IR sensor to determine if there's a cup or not.
    The IR sensor is used to take n number of readings, and the avg reading
    is compared to a preset threshold.  If there is a cup, it will pour,
    but if not, it will hold onto the order and keep checking until a cup
    is put down.

--- Machine.cpp
+++ Machine.cpp
@@ -10,15 +10,18 @@ Machine::Machine() {
 }
 
 const byte Machine::valveTransistorPins[] = {3,4,5,6,8,9};
+const byte Machine::infraredSensorPin = 11;
 
-//setup pins as input/output, and make sure they start at LOW (valves off)
+//setup pins as input/output, and make sure they start at LOW (valves and sensors off)
 void Machine::setPins() {
-  pinMode(pumpTransistor, OUTPUT);
-  digitalWrite(pumpTransistor, LOW);
+  pinMode(infraredSensorPin, OUTPUT);
+  digitalWrite(infraredSensorPin, LOW);
+
   for (byte i = 0; i < bottleCount(); i++) {
     pinMode(valveTransistorPins[i], OUTPUT);
     digitalWrite(valveTransistorPins[i], LOW);
   }
+
   Serial.println("pin mode selection complete");
 }
 
@@ -36,4 +39,27 @@ void Machine::controlValve(valveStatus_t valveStatus, byte bottleNumber) {
     Serial.println("valve is closed!");
     digitalWrite(valveTransistorPins[bottleNumber], LOW);
   }
+}
+
+// Sensor Methods
+const int Machine::presentCupThreshold = 700;
+const int Machine::defaultAverageReadingCount = 10;
+
+//takes as many readings are specified and returns the average
+int Machine::averageReading(int readingsCount) {
+  int i, sumOfSensorValues;
+  while (i < readingsCount) {
+    sumOfSensorValues += analogRead(infraredSensorPin);
+    i++;
+    delay(1); //delay in between reads for stability
+  }
+  return sumOfSensorValues / readingsCount;
+}
+
+
+bool Machine::isCupPresent() {
+  if (averageReading(defaultAverageReadingCount) < presentCupThreshold) {
+    return true;
+  }
+  return false;
 }
